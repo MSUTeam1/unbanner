219a3217c342e44877045ad8dd57ea8923915288
Casey Blanton <cblanto7@users.noreply.github.com>
2017-09-19 16:16:49 -0600

    comments added to studentcontroller

--- src/main/java/unbanner/StudentController.java
+++ src/main/java/unbanner/StudentController.java
@@ -11,6 +11,7 @@ public class StudentController {
     @Autowired
     StudentRepository repository;
 
+    //list all students
     @RequestMapping(value="/students",method=RequestMethod.GET)
     public String StudentsList(Model model) {
         model.addAttribute("students", repository.findAll());
@@ -19,18 +20,20 @@ public class StudentController {
 
     @RequestMapping("/student/{id}")
 
+    //show one student
     public String student(@PathVariable String id, Model model) {
         model.addAttribute("student", repository.findOne(id));
         return "student";
     }
 
-
+    //delete one student
     @RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
     public String student(@PathVariable String id) {
         repository.delete(id);
         return "redirect:/students";
     }
 
+    //update one student
     @RequestMapping(value = "/student/{id}", method = RequestMethod.POST)
     public String student(@ModelAttribute("student") Student student) {
         repository.save(student); //FIX ME I MAKE EVERYTHING NULL
