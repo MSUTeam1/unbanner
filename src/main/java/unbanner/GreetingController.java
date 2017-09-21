de3d8889f5d1012338799c757b17bbac595e8833
Casey Blanton <cblanto7@users.noreply.github.com>
2017-09-21 00:21:48 -0600

    fixed checkStyle warnings

--- src/main/java/unbanner/GreetingController.java
+++ src/main/java/unbanner/GreetingController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class GreetingController {
 
   @RequestMapping("/greeting")
-  public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+  public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World")
+                               String name, Model model) {
     model.addAttribute("name", name);
     return "greeting";
   }
--- src/main/java/unbanner/StudentController.java
+++ src/main/java/unbanner/StudentController.java
@@ -1,11 +1,14 @@
 package unbanner;
 
-
-import unbanner.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import unbanner.Student;
 
 @Controller
 public class StudentController {
