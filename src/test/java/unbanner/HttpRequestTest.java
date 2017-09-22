0055f59f6d55a29ab5aa1421eb2b36c4cc128996
Casey Blanton <cblanto7@users.noreply.github.com>
2017-09-21 18:03:16 -0600

    Http and StudentController tests

---
+++ src/test/java/unbanner/HttpRequestTest.java
@@ -0,0 +1,42 @@
+package unbanner;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HttpRequestTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void indexShouldRespond() throws Exception {
+    this.mockMvc.perform(get("/"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void studentsShouldRespond() throws Exception {
+    this.mockMvc.perform(get("/students"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void studentsNewShouldRespond() throws Exception {
+    this.mockMvc.perform(get("/students/new"))
+        .andExpect(status().isOk());
+  }
+
+  //TODO figure out how to test student/id route
+
+}
