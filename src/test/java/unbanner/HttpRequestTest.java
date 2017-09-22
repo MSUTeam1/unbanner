91e1d884a2eaa69c91dda53bf359762f62798dff
Casey Blanton <cblanto7@users.noreply.github.com>
2017-09-21 20:38:35 -0600

    improved HttpRequestTest

--- src/test/java/unbanner/HttpRequestTest.java
+++ src/test/java/unbanner/HttpRequestTest.java
@@ -2,6 +2,7 @@ package unbanner;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,21 +23,24 @@ public class HttpRequestTest {
   @Test
   public void indexShouldRespond() throws Exception {
     this.mockMvc.perform(get("/"))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(print());
   }
 
   @Test
   public void studentsShouldRespond() throws Exception {
     this.mockMvc.perform(get("/students"))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(print());
   }
 
   @Test
   public void studentsNewShouldRespond() throws Exception {
     this.mockMvc.perform(get("/students/new"))
-        .andExpect(status().isOk());
+        .andExpect(status().isOk())
+        .andDo(print());
   }
 
-  //TODO figure out how to test student/id route
+  //TODO figure out how to test student/id route and also post requests
 
 }
