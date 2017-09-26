436f74af12d35e5e1feed80a72635fdff1ae82fd
Casey Blanton <cblanto7@users.noreply.github.com>
2017-09-25 22:15:24 -0600

    added students_list_test

--- src/test/java/unbanner/StudentControllerTest.java
+++ src/test/java/unbanner/StudentControllerTest.java
@@ -2,21 +2,9 @@ package unbanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InOrder;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
-import static org.mockito.Mockito.*;
-import org.mockito.Mock;
-import static org.hamcrest.Matchers.is;
-
-import org.junit.Test;
-import org.junit.Assert;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -30,39 +18,8 @@ public class StudentControllerTest {
   @Autowired
   private StudentController controller;
 
-  /*
-  @Mock
-  private Student mockStudent1;
-
-  @Mock Student mockStudent2;
-
-  @Mock
-  private StudentRepository mockRepo;
-  */
-
   @Mock
   private Model mockModel;
-
-  private List<Student> mockStudentList;
-
-  @Before
-  public void setupMocks() throws Exception {
-    /*
-    MockitoAnnotations.initMocks(this);
-    mockStudent1.firstName = "Jimmie";
-    mockStudent1.lastName = "Johnson";
-    mockStudent1.studentNum = 48;
-    mockStudent1.id = "Hendrick";
-
-    mockStudent2.firstName = "Kyle";
-    mockStudent2.lastName = "Busch";
-    mockStudent2.studentNum = 18;
-    mockStudent2.id = "Gibbs";
-
-    mockStudentList.add(mockStudent1);
-    mockStudentList.add(mockStudent2);
-    */
-  }
 
   @Test
   public void contextLoads() throws Exception {
@@ -71,8 +28,8 @@ public class StudentControllerTest {
 
   @Test
   public void test_students_list() throws Exception {
-      String ret = controller.studentsList(mockModel);
-      assertThat("students".equals(ret));
+    String ret = controller.studentsList(mockModel);
+    assertThat("students".equals(ret));
   }
 
 
