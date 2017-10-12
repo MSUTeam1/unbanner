407ecde5dfd93527e08074dbc73b1f3d1396ebd2
Casey Blanton <cblanto7@users.noreply.github.com>
2017-10-12 00:39:34 -0600

    added thymeleaf-layout-dialect to project, refactored all exsisting layouts

--- src/main/java/unbanner/Application.java
+++ src/main/java/unbanner/Application.java
@@ -2,10 +2,17 @@ package unbanner;
 
 import java.util.Arrays;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +28,25 @@ public class Application implements CommandLineRunner {
 
   @Autowired
   private SectionRepository sectionRepository;
+
+  @Bean
+  public TemplateResolver templateResolver() {
+    TemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setPrefix("templates/");
+    templateResolver.setCacheable(false);
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode("HTML5");
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    templateEngine.addDialect(new LayoutDialect());
+    return templateEngine;
+  }
+
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
