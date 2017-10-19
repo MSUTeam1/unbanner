8fffd7f3f3a6244549679ee3fe25a9d9ec693ba2
Casey Blanton <cblanto7@users.noreply.github.com>
2017-10-18 23:45:22 -0600

    added WeekdayTest

---
+++ src/test/java/unbanner/WeekdayTest.java
@@ -0,0 +1,13 @@
+package unbanner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class WeekdayTest {
+
+  @Test
+  public void toStringShouldWork(){
+    Assert.assertTrue(Weekday.W.toString().equals("W"));
+  }
+}
