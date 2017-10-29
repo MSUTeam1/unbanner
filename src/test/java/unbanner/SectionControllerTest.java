3b6117268c93073906dc44cdf5bfa73416476222
Mahesh <mkbhattarai1@yahoo.com>
2017-10-29 16:34:23 -0600

    fixed checkstyle

--- src/test/java/unbanner/SectionControllerTest.java
+++ src/test/java/unbanner/SectionControllerTest.java
@@ -1,5 +1,8 @@
 package unbanner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
 import org.bson.types.ObjectId;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,17 +11,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.ui.Model;
-
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
