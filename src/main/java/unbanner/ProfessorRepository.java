b7b47bc5a0692df0c601b4ad3d3db65c7eb99aa2
Nick Barnes <nbarnes7@msudenver.edu>
2017-11-02 12:53:35 -0600

    Still working on professor class and everything that goes with that

---
+++ src/main/java/unbanner/ProfessorRepository.java
@@ -0,0 +1,20 @@
+package unbanner;
+
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+
+public interface ProfessorRepository extends MongoRepository<Professor, String> {
+
+  public Professor findByFirstName(String firstName);
+
+  public List<Professor> findByLastName(String lastName);
+
+  public Professor findByFirstNameAndLastName(String firstName, String lastName);
+
+}
+
+
