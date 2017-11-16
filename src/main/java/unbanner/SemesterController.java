1a596e2a520553956314d0e1cf9fb26bfc0583d2
Brodski <cbrodski@gmail.com>
2017-11-15 23:48:03 -0700

    semester is working, still needs tests

--- src/main/java/unbanner/SemesterController.java
+++ src/main/java/unbanner/SemesterController.java
@@ -22,19 +22,20 @@ public class SemesterController {
     @Autowired
     StudentRepository studentRepository;
 
+    // get all semesters
     @RequestMapping (value = "/semesters", method = RequestMethod.GET)
     public String semestersList(Model model) {
         model.addAttribute("semesters", semesterRepository.findAll());
         return "semesters";
     }
 
-//    @RequestMapping(value = "/courses/new", method = RequestMethod.GET)
-//    public String provideSemester(Model model) {
-//        model.addAttribute("semester",new Semester());
-//        return "create_semester";
-//    }
+    //send to create_semester
+    @RequestMapping(value = "/semesters/new", method = RequestMethod.GET)
+    public String provideSemester(@ModelAttribute("semester") Semester semester) {
+        return "create_semester";
+    }
 
-
+    //create semester
     @RequestMapping (value = "/semesters/new", method = RequestMethod.POST)
     public String newSemester(@ModelAttribute("semester") Semester semester) {
         Semester newSemester = new Semester();
@@ -44,33 +45,32 @@ public class SemesterController {
         return "redirect:/semesters";
     }
 
-    @RequestMapping(value = "/semster/{id}")
+    //get semester
+    @RequestMapping(value = "/semester/{id}")
     public String semester(@PathVariable String id, Model model) {
         model.addAttribute("semester", semesterRepository.findOne(id));
         return "semester";
     }
 
-    //delete method. allows the existence of sections without semesters
+    //delete semester
     @RequestMapping(value = "/semester/{id}", method = RequestMethod.DELETE)
     public String course(@PathVariable String id) {
-        Semester temp = semesterRepository.findOne(id);
-        List<Section> tempSections = sectionRepository.findBySemester(temp);
-        for(Section section : tempSections) {
-            sectionRepository.delete(section);
-        }
+//        Semester temp = semesterRepository.findOne(id);
+//        List<Section> tempSections = sectionRepository.findBySemester(temp);
+        //       for(Section section : tempSections) {
+        //          sectionRepository.delete(section);
+        //     }
         semesterRepository.delete(id);
         return "redirect:/semesters";
     }
 
-    //update? pretty sure this is update
+    //update
     @RequestMapping(value = "/semester/{id}", method = RequestMethod.POST)
     public String semester(@ModelAttribute("semester") Semester semester,
                            @PathVariable String id) {
         Semester mySemester = semesterRepository.findOne(id);
         mySemester.year = semester.year;
         mySemester.season = semester.season;
-        //in not sure how to have it handle the semester list it should be
-        // holding.
         semesterRepository.save(mySemester);
         return "redirect:/semesters";
     }
